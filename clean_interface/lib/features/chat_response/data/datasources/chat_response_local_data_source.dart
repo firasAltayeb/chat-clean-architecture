ef2ff28af60b72481f0515b383e3b01016a483d7
firas <tayebfiras@gmail.com>
2024-04-01 05:07:10 +0900

    implemented local data source code and unit test

--- clean_interface/lib/features/chat_response/data/datasources/chat_response_local_data_source.dart
+++ clean_interface/lib/features/chat_response/data/datasources/chat_response_local_data_source.dart
@@ -1,7 +1,40 @@
-import 'package:clean_interface/core/utils/types.dart';
-import 'package:clean_interface/features/chat_response/data/models/chat_response_model.dart';
+import 'dart:convert';
+
+import '../../../../core/utils/hive_box.dart';
+import '../../../../core/utils/types.dart';
+import '../models/chat_response_model.dart';
 
 abstract class ChatResposeLocalDataSource {
-  Future<ChatResponseList> getChatResponseList();
+  Future<ChatResponseModels> getChatResponseList();
   Future<void> cacheChatResponse(ChatResponseModel chatResponse);
+}
+
+class ChatResposeLocalDataSourceImpl implements ChatResposeLocalDataSource {
+  final LocalCacheBox box;
+
+  ChatResposeLocalDataSourceImpl({
+    required this.box,
+  });
+
+  @override
+  Future<ChatResponseModels> getChatResponseList() {
+    final jsonString = box.getChatResponseListJson();
+    if (jsonString.isNotEmpty) {
+      final jsonResponse = jsonDecode(jsonString) as List<dynamic>;
+      final chatResponseList =
+          jsonResponse.map((i) => ChatResponseModel.fromMap(i)).toList();
+      return Future.value(chatResponseList);
+    } else {
+      final ChatResponseModels temp = [];
+      return Future.value(temp);
+    }
+  }
+
+  @override
+  Future<void> cacheChatResponse(ChatResponseModel chatResponse) async {
+    final cachedList = await getChatResponseList()
+      ..add(chatResponse);
+    final jsonString = jsonEncode(cachedList);
+    await box.setChatResponseListJson(jsonString);
+  }
 }
---
+++ clean_interface/test/features/chat_response/data/datasources/chat_response_local_data_source_test.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+
+import 'package:clean_interface/core/utils/hive_box.dart';
+import 'package:clean_interface/features/chat_response/data/datasources/chat_response_local_data_source.dart';
+import 'package:clean_interface/features/chat_response/data/models/chat_response_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+class MockLocalCacheBox extends Mock implements LocalCacheBox {}
+
+void main() {
+  late ChatResposeLocalDataSourceImpl dataSource;
+  late MockLocalCacheBox mockHiveBox;
+
+  setUp(() {
+    mockHiveBox = MockLocalCacheBox();
+    dataSource = ChatResposeLocalDataSourceImpl(
+      box: mockHiveBox,
+    );
+  });
+
+  final chatResponseList = jsonDecode(fixture('chat_response_list.json'))
+      .map((i) => ChatResponseModel.fromMap(i))
+      .toList();
+
+  test(
+    'should return ChatResponseListJson from Hive when available',
+    () async {
+      when(() => mockHiveBox.getChatResponseListJson())
+          .thenReturn(fixture('chat_response_list.json'));
+      // act
+      final result = await dataSource.getChatResponseList();
+      // assert
+      verify(() => mockHiveBox.getChatResponseListJson()).called(1);
+      expect(result, chatResponseList);
+    },
+  );
+
+  final chatResponseModel = ChatResponseModel(role: 'user', content: 'test');
+  final chatResponses = [chatResponseModel];
+
+  test(
+    "should call hivebox to cache the data",
+    () async {
+      // arrange
+      final expextedJsonStr = jsonEncode(chatResponses);
+      when(() => mockHiveBox.getChatResponseListJson()).thenReturn('');
+      when(() => mockHiveBox.setChatResponseListJson(any()))
+          .thenAnswer((_) => Future.value());
+      // act
+      await dataSource.cacheChatResponse(chatResponseModel);
+      // assert
+      verify(() => mockHiveBox.setChatResponseListJson(expextedJsonStr));
+    },
+  );
+}
